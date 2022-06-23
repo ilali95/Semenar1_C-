4e9b943255cc474e0c406e27752c3e420e5544f6
«isa» <isa.ilandaev@mail.ru>
2022-06-23 22:20:29 +0300

    Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,11 @@
+﻿int num1, num2;
+
+Console.WriteLine("Введите число 1:");
+num1 = Convert.ToInt32(Console.ReadLine()); ;
+Console.WriteLine("Введите число 2:");
+num2 = Convert.ToInt32(Console.ReadLine()); ;
+
+if (num1 == num2 * num2)
+	Console.WriteLine($"{num1} является квадратом числа {num2}");
+else
+	Console.WriteLine($"{num1} не является квадратом числа {num2}");
